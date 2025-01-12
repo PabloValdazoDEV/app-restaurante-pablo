6807ed3ee16f46bfa97c60fda68c6fc56c742939
PabloValdazoDEV <157512231+PabloValdazoDEV@users.noreply.github.com>
2025-01-12 02:05:33 +0100

    Configurar los cambios de estado de los pedidos, y empezar a maquetar

--- router/carta.js
+++ router/carta.js
@@ -20,8 +20,18 @@ router.get('/', async (req, res)=>{
 
 router.post('/crear-pedido', async (req, res)=>{
     const {email, description, quantityAll} = req.body
-    console.log(req.body)
+
     try {
+
+        const pedidosTotales = await prisma.order.findMany({
+            where:{
+                status: 'ENTREGADO'
+            }
+        })
+        if(pedidosTotales.length >= 16){
+            return res.status(500).redirect('/carta/maximos-pedidos')
+        }
+
         const productsArray =  JSON.parse(quantityAll);
         const newOrder = await prisma.order.create({
             data:{
@@ -37,30 +47,49 @@ router.post('/crear-pedido', async (req, res)=>{
                 }
             }
         })
+
+        
+        const pedido = await prisma.order.findUnique({
+            where:{
+                id: newOrder.id
+            },
+            include:{
+                products: {
+                    include:{
+                        product:true
+                    }
+                }
+            }
+            
+        })
+        
+        console.log(pedido)
+
         const mailOptions = {
             from: process.env.GMAIL_USER,
             to: email,
             subject: 'App Restaurante',
-            text: 'Pedido realizado '
+            text: 'Pedido realizado ',
+            html:`
+            <h1>Pedido realizado</h1><br>
+            <h3>Tu pedido esta en ${pedido.status === 'ENTRADA' ? '25 minutos': "15 minutos"}</h3>
+            <p>Has pedido:</p>
+            <ul>
+            ${pedido.products.map(el => `<li>${el.product.name} - Cant. ${el.quantity} - ${el.product.price}€ /ud</li>`).join('')}
+            </ul>
+            <p>Total a pagar: <b>${pedido.products.reduce((acc, value)=> acc + (value.quantity * value.product.price),0)}€</b></p>
+            <p>Para más información sobre tu pedido, haz <a href="http://localhost:3000/carta/info-pedido/${pedido.id}">click aquí</a></p>
+            `
         }
 
        await transporter.sendMail(mailOptions);
-       const queryParams = new URLSearchParams({id: newOrder.id, status: newOrder.status}).toString();
-        res.redirect(`/carta/gracias?${queryParams}`)
+        res.redirect(`/carta/info-pedido/${newOrder.id}`)
     } catch (error) {
         console.error(error)
         res.status(500).redirect('/error-404')
     }
 })
 
-router.get('/gracias', (req,res)=>{
-    const { id, status } = req.query;
-    res.render('pedido-realizado', {
-        layout: 'cliente',
-        id,
-        status
-    })
-})
 
 router.get('/info-pedido/:id', async(req,res)=>{
     const { id } = req.params
@@ -79,17 +108,23 @@ router.get('/info-pedido/:id', async(req,res)=>{
             }
 
         })
-        console.log(pedido.products[0].quantity)
-        console.log(pedido.products[0].product.price)
+       
         res.render('info-pedido',{
             pedido,
+            total: pedido.products.reduce((acc, value)=> acc + (value.quantity * value.product.price),0),
+            layout: 'cliente'
         }
         )
     } catch (error) {
         console.error(error)
         res.status(500).redirect('/error-404')
     }
-   
+})
+
+router.get('/maximos-pedidos', (req, res)=>{
+    res.render('maximos-pedidos',{
+        layout: 'cliente'
+    })
 })
 
 module.exports = router
